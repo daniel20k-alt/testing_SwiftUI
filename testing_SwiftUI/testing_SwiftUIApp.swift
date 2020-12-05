a95ed5a566abab2add8ae08564f29c1046ff8006
Testing_author <testing_beta@mail.com>
2020-12-05 23:54:51 +0200

    Initial Commit

---
+++ testing_SwiftUI/testing_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_SwiftUIApp.swift
+//  testing_SwiftUI
+//
+//  Created by DDDD on 05/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct testing_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
