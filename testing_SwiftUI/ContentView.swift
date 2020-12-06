ceccb46caaeb4ed9a02817dc6e9be663814160af
Testing_author <testing_beta@mail.com>
2020-12-06 23:15:25 +0200

    Implementing modifiers and configuring their order .

--- testing_SwiftUI/ContentView.swift
+++ testing_SwiftUI/ContentView.swift
@@ -5,15 +5,30 @@
 //  Created by DDDD on 05/12/2020.
 //
 
+
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .fontWeight(.bold)
+                .kerning(4.0)
+                .padding()
+          
+            Image(systemName:   "swift")
+                .resizable()
+                .padding(.all)
+                .frame(width: 100.0, height: 100.0)
+                .background(Color.blue)
+
+    
+        }
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
