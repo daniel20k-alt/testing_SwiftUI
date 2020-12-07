3a1397e9e5b4ac0efd36447c01488a810fd61274
Testing_author <testing_beta@mail.com>
2020-12-07 23:55:46 +0200

    Added padding for a more calibrated image.

--- testing_SwiftUI/ContentView.swift
+++ testing_SwiftUI/ContentView.swift
@@ -17,7 +17,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+    SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+        .padding()
   
             Text("Hello, world!")
                 .fontWeight(.bold)
