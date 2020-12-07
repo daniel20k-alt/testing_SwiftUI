e1b2a4f72ffa48661aa9c351b313e9aac5235198
Testing_author <testing_beta@mail.com>
2020-12-07 23:54:23 +0200

    Updating colours by using a ColorPicker as well as changing the opacity by using a Slider.

--- testing_SwiftUI/ContentView.swift
+++ testing_SwiftUI/ContentView.swift
@@ -11,8 +11,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.5
+    
     var body: some View {
         VStack {
+            SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+  
             Text("Hello, world!")
                 .fontWeight(.bold)
                 .kerning(4.0)
@@ -22,7 +28,8 @@ struct ContentView: View {
                 .resizable()
                 .padding(.all)
                 .frame(width: 100.0, height: 100.0)
-                .background(Color.blue)
+                .opacity(swiftyOpacity)
+                .background(swiftyColor)
 
     
         }
@@ -32,5 +39,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SwiftyControls: View {
+    
+    @Binding var swiftyColor: Color
+    @Binding var swiftyOpacity: Double
+    
+    var body: some View {
+        VStack {
+            ColorPicker("Swifty color", selection: $swiftyColor)
+            Slider(value: $swiftyOpacity, in: 0...1)
+                .accentColor(swiftyColor)
+        }
     }
 }
