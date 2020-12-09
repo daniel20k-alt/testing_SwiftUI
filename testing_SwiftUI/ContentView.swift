7ed9719e21118a0efe68c4e153b565fd545331c3
Testing_author <testing_beta@mail.com>
2020-12-09 23:04:54 +0200

    Added another view using HStacks.

--- testing_SwiftUI/ContentView.swift
+++ testing_SwiftUI/ContentView.swift
@@ -17,36 +17,45 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            VStack { SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
-        .padding()
-  
-            Text("Hello, world!")
-                .fontWeight(.bold)
-                .kerning(4.0)
-                .padding()
-            
-            Text("This is an image, of course")
-                .fontWeight(.bold)
-                .kerning(4.0)
-                .padding()
-          
-            Image(systemName:   "swift")
-                .resizable()
-                .padding(.all)
-                .frame(width: 100.0, height: 100.0)
-                .opacity(swiftyOpacity)
-                .background(swiftyColor)
-
-        }
+            HStack {
+                Thumbnail()
+                VStack {
+                    SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+                    .padding()
+                    
+                    Text("Hello, world!")
+                        .fontWeight(.bold)
+                        .kerning(4.0)
+                        .padding()
+                    
+                    Text("This is an image, of course")
+                        .fontWeight(.bold)
+                        .kerning(4.0)
+                        .padding()
+                    
+                    Image(systemName:   "swift")
+                        .resizable()
+                        .padding(.all)
+                        .frame(width: 100.0, height: 100.0)
+                        .opacity(swiftyOpacity)
+                        .background(swiftyColor)
+                    
+                }
+            }
             Buttonlabel(title: "Next", systemImage: "square.and.arrow.down.fill")
         }
         
+        
     }
 }
- 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
+        Thumbnail()
+            .padding()
+            .previewLayout(.fixed(width:250, height: 250))
     }
 }
 
@@ -64,6 +73,39 @@ struct SwiftyControls: View {
     }
 }
 
+
+struct Thumbnail: View {
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.accentColor)
+                    .aspectRatio(contentMode: .fit)
+                    .shadow(radius: 15)
+                
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.white)
+                    .opacity(0.5)
+            }
+            VStack(alignment: .leading) {
+                Text("it works")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Text("This too, yay")
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding()
+        }
+    }
+}
+
+
+
 struct Buttonlabel: View {
     let title: String
     let systemImage: String
@@ -80,7 +122,7 @@ struct Buttonlabel: View {
                     .padding(.vertical)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(15)
-                }
+            }
         }
     }
 }
